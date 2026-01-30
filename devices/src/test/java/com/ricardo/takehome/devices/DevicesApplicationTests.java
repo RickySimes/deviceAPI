43d74373d2b76f5bd3732ff609b2a000c6ddf122
Simoes Ricardo (Telenor Sverige AB) <rs995413@telenor.se>
2026-01-30 17:06:40 +0100

    initialized project and folder structure

---
+++ devices/src/test/java/com/ricardo/takehome/devices/DevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.takehome.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
